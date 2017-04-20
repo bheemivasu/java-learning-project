525911ee075cb42c752de88e27eff50259973946
bheemivasu <vasureddy122@gmail.com>
2017-04-20 19:25:13 +0530

    Inheritance animals examples

---
+++ src/com/learning/inheritance/concept/Animalsclass.java
@@ -0,0 +1,15 @@
+package com.learning.inheritance.concept;
+
+/**
+ * Created by bheemivasu on 20-04-2017.
+ */
+public class Animalsclass {
+
+
+    public void animals(){
+        System.out.println("Animals super class");
+    }
+    public void animalsTypes(){
+        System.out.println("Animals are diff types");
+    }
+}
---
+++ src/com/learning/inheritance/concept/Dogsubclass.java
@@ -0,0 +1,21 @@
+package com.learning.inheritance.concept;
+
+/**
+ * Created by bheemivasu on 20-04-2017.
+ */
+public class Dogsubclass extends Animalsclass {
+    public  void animalSound(){
+        System.out.println("Dog sound");
+    }
+    public void dogColor(){
+        System.out.println("Dog color is black");
+    }
+
+    public static void main(String[] args) {
+        Dogsubclass dog =new Dogsubclass();
+        dog.animalSound();
+        dog.dogColor();
+        dog.animals();
+        dog.animalsTypes();
+    }
+}
